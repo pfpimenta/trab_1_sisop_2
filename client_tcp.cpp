f8942e0343c0e7fdf55f1284133c13d36edf324f
pfpimenta <pedrofolettopimenta@gmail.com>
2021-03-29 00:22:12 -0300

    server-client communication + threads for server

--- client_tcp.cpp
+++ client_tcp.cpp
@@ -10,34 +10,139 @@
 #include <netdb.h> 
 
 #define PORT 4000
+#define BUFFER_SIZE 256
+#define MESSAGE_SIZE 128
 
 // TODO : put in another file
 typedef struct __packet{
-    uint16_t type; // Tipo do pacote (p.ex. DATA | CMD)
+    uint16_t type; // Tipo do pacote:
+        // 0 - CONNECT (username_to_login, seqn)
+        // 1 - FOLLOW (username_to_follow, seqn)
+        // 2 - SEND (message_to_send, seqn)
+        // 3 - MSG (username, message_sent, seqn)
+        // 4 - ACK (seqn)
+        // 5 - ERROR (seqn)
     uint16_t seqn; // Número de sequência
     uint16_t length; // Comprimento do payload
-    //uint16_t timestamp; // Timestamp do dado // provavelmente nao precisamos disso
     const char* _payload; // Dados da mensagem
-        // CONNECT username_to_login
-        // FOLLOW username_to_follow
-        // SEND message_to_send
-        // MSG username timestamp message_sent
-        // ACK seqn
-        // ERROR seqn
 } packet;
 
+const char* get_packet_type_string(int packet_type)
+{
+  switch (packet_type)
+  {
+  case 0:
+    return "CONNECT";
+    break;
+  case 1:
+    return "FOLLOW";
+    break;
+  case 2:
+    return "SEND";
+    break;
+  case 3:
+    return "MSG";
+    break;
+  case 4:
+    return "ACK";
+    break;
+  case 5:
+    return "ERROR";
+    break;
+  default:
+    printf("ERROR: invalid packet type\n");
+    return NULL;
+    break;
+  }
+}
+
+int get_packet_type(char* packet_type_string)
+{
+
+  if (strcmp(packet_type_string, "CONNECT") == 0) 
+  {
+    return 0;
+  } 
+  else if (strcmp(packet_type_string, "FOLLOW") == 0)
+  {
+    return 1;
+  }
+  else if (strcmp(packet_type_string, "SEND") == 0)
+  {
+    return 2;
+  }
+  else if (strcmp(packet_type_string, "MSG") == 0)
+  {
+    return 3;
+  }
+  else if (strcmp(packet_type_string, "ACK") == 0)
+  {
+    return 4;
+  }
+  else if (strcmp(packet_type_string, "ERROR") == 0)
+  {
+    return 5;
+  }
+  else
+  {
+		printf("ERROR: unkown packet type");
+    exit(0);
+  }
+}
+
+packet create_packet(char* message, int packet_seqn, int packet_type)
+{
+  packet packet_to_send;
+  packet_to_send._payload = message;
+  packet_to_send.seqn = packet_seqn; 
+  packet_to_send.length = strlen(packet_to_send._payload);
+  packet_to_send.type = packet_type;
+  return packet_to_send;
+}
+
+// serializes the packet and puts it in the buffer
+void serialize_packet(packet packet_to_send, char* buffer)
+{
+  bzero(buffer, sizeof(buffer));
+  snprintf(buffer, BUFFER_SIZE, "%u,%u,%u,%s",
+          packet_to_send.seqn, packet_to_send.length, packet_to_send.type, packet_to_send._payload);
+}
+
+// asks the user to write a message and then sends it to the server
+int send_message(int sockfd, int seqn)
+{
+  packet packet_to_send;
+  char buffer[BUFFER_SIZE];
+  char message[MESSAGE_SIZE];
+
+  printf("Please enter your message:");
+  bzero(message, sizeof(message));  
+  fgets(message, MESSAGE_SIZE, stdin);
+  message[strlen(message)-1] = 0; // remove '\n'
+  packet_to_send = create_packet(message, seqn, 3);
+  serialize_packet(packet_to_send, buffer);
+  int n = write(sockfd, buffer, strlen(buffer));
+  if (n < 0) 
+  {
+    printf("ERROR writing to socket\n");
+    return 0;
+  }
+  else
+    return 1;
+}
 
 int main(int argc, char *argv[])
 {
   int sockfd, n;
   struct sockaddr_in serv_addr;
   struct hostent *server;
+  packet packet_to_send;
 
-  char buffer[256];
-  char message[256];
+  char buffer[BUFFER_SIZE];
+  char message[MESSAGE_SIZE];
 
   if (argc < 2) {
-		fprintf(stderr,"usage %s hostname\n", argv[0]);
+		fprintf(stderr,"usage: %s hostname\n", argv[0]);
 		exit(0);
   }
 	
@@ -48,7 +153,7 @@ int main(int argc, char *argv[])
   }
     
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
-      printf("ERROR opening socket\n");
+    printf("ERROR opening socket\n");
     
   // assign ip and port to socket
 	serv_addr.sin_family = AF_INET;     
@@ -60,49 +165,37 @@ int main(int argc, char *argv[])
 	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
     printf("ERROR connecting\n");
 
-  // put the message in a packet
-  packet packet_to_send;
-  size_t payload_length = snprintf(message, 256, "mensagem DEBUGadora dos packet");
-  packet_to_send._payload = message;
-  packet_to_send.seqn = 0; 
-  packet_to_send.length = strlen(packet_to_send._payload);
-  packet_to_send.type = 0; // TODO botar o que aqui?
-  
-  // put packet in a buffer
-  // TODO
-  bzero(buffer, sizeof(buffer));
-
-
 	int seqn = 0;
 
   //// send CONNECT message
+  printf("Please enter your username:");
+  bzero(message, sizeof(message));  
+  fgets(message, MESSAGE_SIZE, stdin);
+  message[strlen(message)-1] = 0; // remove '\n'
   /* write buffer in the socket */
-  bzero(buffer, sizeof(buffer));
-  size_t buffer_length = snprintf(buffer, 256, "CONNECT,username");
+  packet_to_send = create_packet(message, seqn, 0);
+  seqn++;
+  serialize_packet(packet_to_send, buffer);
   n = write(sockfd, buffer, strlen(buffer));
   if (n < 0) 
     printf("ERROR writing to socket\n");
   /* read from the socket */
-  n = read(sockfd, buffer, 256);
+  n = read(sockfd, buffer, BUFFER_SIZE);
   if (n < 0) 
     printf("ERROR reading from socket\n");
 
 
-  //// send MSG message
+  ///////////////////////////////////
+  //// send MSG messages
   while(1){
-    /* write buffer in the socket */
-		bzero(buffer, sizeof(buffer));
-		snprintf(buffer, 14, "MSG, %08d", seqn);
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0) 
-      printf("ERROR writing to socket\n");
-    
+    send_message(sockfd, seqn);
+
     /* read from the socket */
-    n = read(sockfd, buffer, 256);
+    n = read(sockfd, buffer, BUFFER_SIZE);
     if (n < 0) 
       printf("ERROR reading from socket\n");
-  
-    printf("%s\n",buffer);
+    printf("Received from server: %s\n",buffer);
+
     seqn++;
  }
  
--- server_tcp.cpp
+++ server_tcp.cpp
@@ -7,21 +7,26 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
+
 
 #define PORT 4000
+#define MAX_THREADS 15 // maximum number of threads allowed
+#define BUFFER_SIZE 256
+#define MESSAGE_SIZE 128
 
+// TODO : put in another file
 typedef struct __packet{
-    uint16_t type; // Tipo do pacote (p.ex. DATA | CMD)
+    uint16_t type; // Tipo do pacote:
+        // 0 - CONNECT (username_to_login, seqn)
+        // 1 - FOLLOW (username_to_follow, seqn)
+        // 2 - SEND (message_to_send, seqn)
+        // 3 - MSG (username, message_sent, seqn)
+        // 4 - ACK (seqn)
+        // 5 - ERROR (seqn)
     uint16_t seqn; // Número de sequência
     uint16_t length; // Comprimento do payload
-    //uint16_t timestamp; // Timestamp do dado // provavelmente nao precisamos disso
     const char* _payload; // Dados da mensagem
-        // CONNECT username_to_login
-        // FOLLOW username_to_follow
-        // SEND message_to_send
-        // MSG username timestamp message_sent
-        // ACK seqn
-        // ERROR seqn
 } packet;
 
 typedef struct __notification{
@@ -36,10 +41,10 @@ typedef struct __notification{
 void read_message(int newsockfd, char* buffer)
 {
 	// make sure buffer is clear	
-  	bzero(buffer, 256);
+  	bzero(buffer, BUFFER_SIZE);
 	/* read from the socket */
     int n;
-	n = read(newsockfd, buffer, 256);
+	n = read(newsockfd, buffer, BUFFER_SIZE);
 	if (n < 0) 
 		printf("ERROR reading from socket");
 }
@@ -61,16 +66,21 @@ int setup_socket()
     struct sockaddr_in serv_addr;
 
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+	{
         printf("ERROR opening socket");
-	
+        exit(-1);
+	}
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	bzero(&(serv_addr.sin_zero), 8);
     
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+	{
 		printf("ERROR on binding");
-    
+        exit(-1);
+	}
+
 	listen(sockfd, 5);
 
     return sockfd;
@@ -89,41 +99,97 @@ int accept_connection(int sockfd)
 	return newsockfd;
 }
 
+// serializes the packet and puts it in the buffer
+void serialize_packet(packet packet_to_send, char* buffer)
+{
+  bzero(buffer, sizeof(buffer));
+  snprintf(buffer, BUFFER_SIZE, "%u,%u,%u,%s",
+          packet_to_send.seqn, packet_to_send.length, packet_to_send.type, packet_to_send._payload);
+}
+
+packet create_packet(char* message, int packet_seqn, int packet_type)
+{
+  packet packet_to_send;
+  packet_to_send._payload = message;
+  packet_to_send.seqn = packet_seqn; 
+  packet_to_send.length = strlen(packet_to_send._payload);
+  packet_to_send.type = packet_type;
+  return packet_to_send;
+}
+
+// controls the client inputs received by socket
+void * socket_thread(void *arg) {
+	int socket = *((int *)arg);
+	int size = 0;
+	int reference_seqn;
+	char payload[MESSAGE_SIZE];
+	char client_message[BUFFER_SIZE];
+	char reply[BUFFER_SIZE];
+	packet packet_to_send;
+
+	// print pthread id
+	pthread_t thread_id = pthread_self();
+	printf("Socket opened in thread %d\n", (int)thread_id);
+
+	int seqn = 0;
+	do{
+		// receive message
+  		bzero(client_message, sizeof(client_message));
+		size = recv(socket, client_message, BUFFER_SIZE, 0); 
+		client_message[size] = '\0';
+		printf("Thread %d - Received message: %s\n", (int)thread_id, client_message);
+
+		// TODO : treat received message
+		// usar strtok
+		// fazer um switch case
+
+		// send ACK
+		reference_seqn = 0; // TODO
+  		bzero(payload, sizeof(payload));
+		snprintf(payload, MESSAGE_SIZE, "%d", reference_seqn);
+		packet_to_send = create_packet(payload, seqn, 4);
+s		serialize_packet(packet_to_send, reply);
+		write_message(socket, reply);
+		seqn++;
+  	}while (size != 0);
+
+	printf("Exiting socket thread: %d\n", (int)thread_id);
+	close(socket);
+	pthread_exit(NULL);
+}
 
 int main(int argc, char *argv[])
 {
 	int sockfd;
 	int newsockfd, n;
-	// socklen_t clilen;
-	char buffer[256];
+	socklen_t clilen;
+	char buffer[BUFFER_SIZE];
+  	char message[MESSAGE_SIZE];
 	struct sockaddr_in serv_addr, cli_addr;
-	
-    // setup socket
+	packet packet_to_send;
+  	pthread_t tid[MAX_THREADS];
+
+    // setup main socket
     sockfd = setup_socket();
-    
-    // receive request / init connection
-	newsockfd = accept_connection(sockfd);
 
-	// loop: receive request and send reply
-	int seqn = 0;
-  	while(1){    
-		/* read from the socket */
-		read_message(newsockfd, buffer);
-		printf("Received message: %s\n", buffer);
+	// loop that accepts new connections and allocates threads to deal with them
+    int i = 0;
+	while(1) {
+		// accept new connection in a new socket
+		clilen = sizeof(struct sockaddr_in);
+		if ((newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) == -1) 
+			printf("ERROR on accept");
 
-		// TODO : treat received message
-		// fazer um switch case
-		
-		/* write in the socket */
-		bzero(buffer, sizeof(buffer));
-		// strlen("ACK, 12345678") == 14
-		snprintf(buffer, 14, "ACK, %08d", seqn);
-		printf("Sending message: %s\n", buffer);
-		write_message(newsockfd, buffer);
-		seqn++;
+		// create thread for the new socket
+		if (pthread_create(&tid[i], NULL, socket_thread, &newsockfd) != 0 ) {
+			printf("Failed to create thread\n");
+			exit(-1);
+		}
+
+		// TODO: codigo pra limitar o numero maximo de threads
+		// e fechar threads que ja terminaram
 	}
 
-	close(newsockfd);
 	close(sockfd);
 	return 0; 
 }
